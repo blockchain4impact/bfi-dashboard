303e4e6e57f10793c405bbb7eb959cf57a2401a9
blockchain4impact <165136559+blockchain4impact@users.noreply.github.com>
2024-04-27 11:32:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://bfi-rho.vercel.app",
+      "https://bfi-dashboard.vercel.app",
       "http://localhost:3000",
       "https://dashboard.blockchainforimpact.in",
     ],
